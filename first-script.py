945394d86cbe56411a0ebc544410f60632a5cfac
rahulrdb18 <rahulrdb18@gmail.com>
2017-05-21 03:29:22 +0530

    added maps

--- first-script.py
+++ first-script.py
@@ -2,8 +2,10 @@ from osgeo import gdal
 from osgeo import osr
 import sys
 
-# open dataset
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
 
+# show error exceptions on console
 gdal.UseExceptions()
 
 try:
@@ -12,26 +14,24 @@ except RuntimeError:
     print("unable to open the tiff file")
     sys.exit(1)
 
-print("\n\n-------metadata---------\n\n")
+print("\n-------metadata---------\n")
 
 print(ds.GetMetadata())
-band = ds.GetRasterBand(1)
-arrayy = band.ReadAsArray()
-nodata = band.GetNoDataValue()
 
-print(arrayy)
-print(nodata)
+#
+#
+# I think we don't have the data related to Projections
+#
+#
+# print("\n-------projectionPrettyWKT---------\n")
 
-
-print("\n\n-------projectionPrettyWKT---------\n\n")
-
-raster_wkt = ds.GetProjection()
-spatial_ref = osr.SpatialReference()
-spatial_ref.ImportFromWkt(raster_wkt)
-print(spatial_ref.ExportToPrettyWkt())
+# raster_wkt = ds.GetProjection()
+# spatial_ref = osr.SpatialReference()
+# spatial_ref.ImportFromWkt(raster_wkt)
+# print(spatial_ref.ExportToPrettyWkt())
 
 
-print("\n\n-------Raster data---------\n\n")
+print("\n-------Raster data---------\n")
 
 try:
     num_band = ds.RasterCount
@@ -45,16 +45,48 @@ print(num_band)
 for i in range(1, num_band):
     band = ds.GetRasterBand(i)
     print(band)  # band 0 might not be printable
-    print("\n\n-------GEO INFO data---------\n\n")
+    print("\n-------GEO INFO data---------\n")
     typpe = gdal.GetDataTypeName(band.DataType)
-    print("Band Type="+typpe)
-    print(ds.RasterXSize)
-    print(band.YSize)
-    print(ds.GetDriver().LongName)
-    print(ds.GetGeoTransform())
+    print("Band Type=" + typpe)
 
-    scanline = band.ReadRaster(0, 0, band.XSize, 1, band.XSize, 1, typpe)
-    print(scanline)
+    arrayy = band.ReadAsArray()
+    nodata = band.GetNoDataValue()
+
+    print(arrayy)  # Data Matrix
+    print(nodata)
+
+    print("\n-------X Size---------")
+    print(ds.RasterXSize)
+    print("\n-------Y size---------")
+    print(band.YSize)
+    print("\n-------name---------")
+    print(ds.GetDriver().LongName)
+    print("\n-------colorInterpret---------")
+    print(band.GetColorInterpretation())
+    print("\n-------Color Table---------")
+    print(band.GetRasterColorTable())
+    print("\n-------GEO Tranform data---------")
+    print(ds.GetGeoTransform())
+    print("\n-------get histogram---------")
+    print(band.GetDefaultHistogram())
+    print("\n-------RasterAttriTable data---------")
+    print(band.GetDefaultRAT())
+
+    # scanline = band.ReadRaster(0, 0, band.XSize, 1, band.XSize, 1, typpe)
+    # print(scanline)
+
+    # setup Lambert Conformal basemap.
+    m = Basemap(width=12000000, height=9000000, projection='lcc',
+                resolution='c', lat_1=45., lat_2=55, lat_0=50, lon_0=-107.)
+    # draw coastlines.
+    m.drawcoastlines()
+    # draw a boundary around the map, fill the background.
+    # this background will end up being the ocean color, since
+    # the continents will be drawn on top.
+    m.drawmapboundary(fill_color='aqua')
+    # fill continents, set lake color same as ocean color.
+    m.fillcontinents(color='coral', lake_color='aqua')
+    plt.show()
 
     print(str(i))
 
